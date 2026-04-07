be9b2e9433f570efe877d55e29cde32aa65fe44f
Tom Fasquelle <tom.fasquelle@etu.univ-lyon1.fr>
2026-04-07 23:08:08 +0200

    moving files to prepare frontend part

--- src/webservice/index.php
+++ src/webservice/index.php
@@ -1,1 +1,24 @@
-Webservice / API
+<?php
+if (session_status() === PHP_SESSION_NONE){
+    session_start();
+}
+$page = $_GET['page'] ?? 'login';
+
+switch ($page) {
+    case 'dashboard':
+        require_once 'dashboard.php';
+        break;
+
+    case 'login':
+        require_once 'login.php';
+        break;
+
+    case 'logout':
+        require_once 'logout.php';
+        break;
+
+    default:
+        require_once 'login.php';
+        break;
+}
+?>
